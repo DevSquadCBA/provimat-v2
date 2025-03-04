cf926f55c443eabc30a2f1535bc6238df5bc6039
sirferra <sir.leandroferra@gmail.com>
2025-03-04 01:05:08 -0300

    feat: toast implementación

--- src/store.ts
+++ src/store.ts
@@ -2,18 +2,21 @@ import {configureStore} from '@reduxjs/toolkit';
 import {sidebarSlice} from './reducers/sidebarSlice';
 import { localDataSlice } from './reducers/localDataReducer';
 import { modalsSlice } from './reducers/modalsSlice';
+import {toastSlice} from './reducers/toastSlice';
 
 export type reducers = {
     sideBar: typeof sidebarSlice
     localData: typeof localDataSlice
     modalsSlice: typeof modalsSlice
+    toastSlice: typeof toastSlice
 }
 
 export const store = configureStore({
     reducer:{
         sideBar: sidebarSlice.reducer,
         localData: localDataSlice.reducer,
-        modalsSlice: modalsSlice.reducer
+        modalsSlice: modalsSlice.reducer,
+        toastSlice: toastSlice.reducer
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
