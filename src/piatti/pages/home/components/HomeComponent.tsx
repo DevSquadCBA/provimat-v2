ed3e74c99ae605c952c9c872a1e86bc2ccd1a236
sirferra <sir.leandroferra@gmail.com>
2025-03-07 20:58:20 -0300

    feat: home component 100%

--- src/piatti/pages/home/components/HomeComponent.tsx
+++ src/piatti/pages/home/components/HomeComponent.tsx
@@ -2,7 +2,7 @@ import { IClient,  ISale } from "@/interfaces/dbModels";
 import { ErrorResponse } from "@/interfaces/Errors";
 import { showToast } from "@/reducers/toastSlice";
 import API from "@/services/API";
-import { getUserData, removeToken } from "@/services/common";
+import { formatPrice, getUserData, removeToken } from "@/services/common";
 import moment from "moment";
 import { Chart } from "primereact/chart";
 import { Skeleton } from "primereact/skeleton";
@@ -11,6 +11,7 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import productIcon from '@/assets/products.svg';
 import truckIcon from '@/assets/logoproovedores.svg';
+import salesIcon from '@/assets/sidebar/logopresupuestos.svg';
 import './HomeComponent.scss';
 type Metrics ={
     mostDemandProducts: {productName:string,total_quantity:number}[],
@@ -116,102 +117,115 @@ export function HomeComponent(){
                 <Skeleton className="mb-5 w-3" height="200px" borderRadius="16px"></Skeleton>
                 <Skeleton className="mb-5 w-3" height="200px" borderRadius="16px"></Skeleton>
                 <Skeleton className="mb-5 w-3" height="200px" borderRadius="16px"></Skeleton>
-
-                
             </div>
             </>
         }
         {metrics &&
             <div className="home-container flex flex_row w-100 space-between text-left">
-            <div className="next-deliveries-column">
-                    <h3>Proximas entregas</h3>
-                    <div className="closest-next-delivery">
-                        <div className="truckIcon-container w-100">
-                            <img src={truckIcon} className="truckIcon" alt="truck" />
+                <div className="next-deliveries-column">
+                        <h3>Proximas entregas</h3>
+                        <div className="closest-next-delivery">
+                            <div className="truckIcon-container w-100">
+                                <img src={truckIcon} className="truckIcon" alt="truck" />
+                            </div>
+                            <div className="closest-next-delivery-info w-100">
+                                <p className="date">{moment(metrics.nextDeliverySales[0]?.day).format('DD/MM')}</p>
+                                {metrics.nextDeliverySales[0]?.sales.slice(0,3).map(sale=>(
+                                    <p className="clientName"><span>{sale.client.name}</span></p>
+                                ))}
+                                {metrics.nextDeliverySales[0]?.sales.length==3 
+                                ? <p className="clientName">+1 cliente</p>
+                                : metrics.nextDeliverySales[0]?.sales.length>3  
+                                    ? <p className="clientName">+{metrics.nextDeliverySales[0]?.sales.length-2} clientes</p>
+                                    :<></>}
+                            </div>
                         </div>
-                        <div className="closest-next-delivery-info w-100">
-                            <p className="date">{moment(metrics.nextDeliverySales[0]?.day).format('DD/MM')}</p>
-                            {metrics.nextDeliverySales[0]?.sales.slice(0,3).map(sale=>(
-                                <p className="clientName"><span>{sale.client.name}</span></p>
+                        <div className="next-deliveries">
+                            {metrics.nextDeliverySales.slice(1,3).map(days=>(
+                                <div className="next-delivery">
+                                    <p className="date">{moment(days.sales[0].deadline).format('DD/MM')}</p>
+                                    {days.sales.map(sale=>(
+                                        <p className="clientName">{sale.client.name}</p>
+                                    ))}
+                                </div>
                             ))}
-                            {metrics.nextDeliverySales[0]?.sales.length==3 
-                            ? <p className="clientName">+1 cliente</p>
-                            : metrics.nextDeliverySales[0]?.sales.length>3  
-                                ? <p className="clientName">+{metrics.nextDeliverySales[0]?.sales.length-2} clientes</p>
-                                :<></>}
                         </div>
-                    </div>
-                    <div className="next-deliveries">
-                        {metrics.nextDeliverySales.slice(1,3).map(days=>(
-                            <div className="next-delivery">
-                                <p className="date">{moment(days.sales[0].deadline).format('DD/MM')}</p>
-                                {days.sales.map(sale=>(
-                                    <p className="clientName">{sale.client.name}</p>
+                </div>
+                <div className="month-sales-column">
+                        <h3>Ventas Mensuales</h3>
+                        <div className="card">
+                        <div className="month-sales w-100">
+                            <Chart type="line" 
+                                id="month-sales"
+                                data={{
+                                    labels:[...new Set(metrics.salesGraph.flatMap(year => year.months.map(month => month.month)))],
+                                    datasets:
+                                        metrics.salesGraph.map(year=>({
+                                        type:'line',
+                                        label:'Ventas de '+year.year,
+                                        data: year.months.map(month => month.total),
+                                        tension: 0.4,
+                                        borderColor: colorByYear(year.year as 2024|2025)
+                                    }))
+                                }} 
+                                options={{
+                                    maintainAspectRatio: false,
+                                    aspectRatio: 1.3,
+                                    plugins: {
+                                        tooltips: {
+                                            mode: 'index',
+                                            intersect: true
+                                        },
+                                        legend: {
+                                            labels: {
+                                                color: '#1F5D53'
+                                            }
+                                        }
+                                    },
+                                    scales: {
+                                        x: {
+                                            stacked: true,
+                                            ticks: {
+                                                color: '#1F5D5350'
+                                            },
+                                            grid: {
+                                                color: 'transparent'
+                                            }
+                                        },
+                                        y: {
+                                            stacked: false,
+                                            ticks: {
+                                                color: '#4CB3A050'
+                                            },
+                                            grid: {
+                                                color: 'transparent'
+                                            }
+                                        }
+                                    }
+                                }} />
+                        </div>
+                        <div className="month-sales-legend">
+                            <img src={salesIcon} alt="sales" />
+                            <div className="month-sales-legends">
+                                {metrics.salesGraph.map(year=>(
+                                    <div className="month-sales-legend-item" style={{ color: colorByYear(year.year as 2024|2025) }}>
+                                        <p>{year.year}</p>
+                                        <p>${formatPrice(year.months.reduce((total, month) => total + month.total, 0))}</p>
+                                    </div>
                                 ))}
                             </div>
-                        ))}
-                    </div>
-                </div>
-                <div className="month-sales-column w-25" style={{width: '30%'}}>
-                    <h3>Ventas Mensuales</h3>
-                    <div className="month-sales w-100">
-                        <Chart type="line" 
-                            id="month-sales"
-                            data={{
-                                labels:[...new Set(metrics.salesGraph.flatMap(year => year.months.map(month => month.month)))],
-                                datasets:
-                                    metrics.salesGraph.map(year=>({
-                                    type:'line',
-                                    label:'Ventas de '+year.year,
-                                    data: year.months.map(month => month.total),
-                                    tension: 0.4,
-                                    borderColor: colorByYear(year.year as 2024|2025)
-                                }))
-                            }} 
-                            options={{
-                                maintainAspectRatio: false,
-                                aspectRatio: 1.3,
-                                plugins: {
-                                    tooltips: {
-                                        mode: 'index',
-                                        intersect: true
-                                    },
-                                    legend: {
-                                        labels: {
-                                            color: '#1F5D53'
-                                        }
-                                    }
-                                },
-                                scales: {
-                                    x: {
-                                        stacked: true,
-                                        ticks: {
-                                            color: '#1F5D5350'
-                                        },
-                                        grid: {
-                                            color: 'transparent'
-                                        }
-                                    },
-                                    y: {
-                                        stacked: false,
-                                        ticks: {
-                                            color: '#4CB3A050'
-                                        },
-                                        grid: {
-                                            color: 'transparent'
-                                        }
-                                    }
-                                }
-                            }} />
+                        </div>
                     </div>
                 </div>
                 <div className="products-with-hight-demand">
                     <h3>Productos con ALTA DEMANDA</h3>
                     <div className="products-with-hight-demand-info">
-                        {metrics.mostDemandProducts.map(product=>(
+                        {metrics.mostDemandProducts.slice(0,5).map(product=>(
                             <div className="product-with-hight-demand">
-                                <div>
-                                    <img src={productIcon} alt="product" />
+                                <div className="product-with-hight-demand-icon">
+                                    <div className="icon-container">
+                                        <img src={productIcon} alt="product" />
+                                    </div>
                                 </div>
                                 <div className="product-with-hight-demand-name">
                                     {product.productName}
